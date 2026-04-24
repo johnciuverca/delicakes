f40e21e76308dc086629c7a37e44f20ee31223f5
johnciuverca <johnciuverca@gmail.com>
2026-04-24 16:03:28 +0300

    feat(atuh): add initial auth middleware and prepare protected routes

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,30 +1,40 @@
-import 'dotenv/config';
+import "dotenv/config";
 
-import express, { type Request, type Response } from 'express';
-import cors from 'cors';
+import express, { type Request, type Response } from "express";
+import cors from "cors";
 
-import transactionRoutes from './model/transactions.js';
-import { addRecipeHandler, deleteRecipesHandler, getRecipesHandler } from './handlers/recipes.js';
+import transactionRoutes from "./model/transactions.js";
+import {
+  addFavoriteRecipeHandler,
+  addRecipeHandler,
+  deleteRecipesHandler,
+  getFavoritesRecipesHandler,
+  getRecipesHandler,
+  removeFavoriteRecipeHandler,
+} from "./handlers/recipes.js";
+import { authMiddleware } from "./middleware/authMiddleware.js";
 
 const app = express();
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3100;
-const ENVIRONMENT = process.env.ENVIRONMENT || 'DEV';
+const ENVIRONMENT = process.env.ENVIRONMENT || "DEV";
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (_req: Request, res: Response) => {
-  res.json({ message: 'Delicakes Money Tracker API' });
+app.get("/", (_req: Request, res: Response) => {
+  res.json({ message: "Delicakes Money Tracker API" });
 });
 
-app.use('/api/transactions', transactionRoutes);
+app.use("/api/transactions", transactionRoutes);
 
-app.post('/api/recipes', addRecipeHandler);
+app.get("/api/recipes", getRecipesHandler);
+app.post("/api/recipes", authMiddleware, addRecipeHandler);
+app.delete("/api/recipes", authMiddleware, deleteRecipesHandler);
 
-app.get('/api/recipes', getRecipesHandler);
-
-app.delete('/api/recipes', deleteRecipesHandler);
+app.get("/api/favorites", getFavoritesRecipesHandler);
+app.post("/api/favorites", authMiddleware, addFavoriteRecipeHandler);
+app.delete("/api/favorites", authMiddleware, removeFavoriteRecipeHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT} in ${ENVIRONMENT} mode`);
